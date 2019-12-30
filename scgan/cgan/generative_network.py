5a67bd7de5bf1300a504ab24346cb752f64833e7
Maciej Adamiak <maciej.adamiak@softwaremill.com>
2019-12-30 09:59:43 +0100

    gdal operations update

--- scgan/cgan/generative_network.py
+++ scgan/cgan/generative_network.py
@@ -15,11 +15,11 @@ class GenerativeNetwork():
         self.optimizer = optimizer
 
     def conv2d(self, initial_layer: Layer, filters: int, kernel_size: int = 4, strides: int = 2,
-               momentum: Optional[float] = None):
+               dropout_rate: Optional[float] = None, momentum: Optional[float] = None):
         down_conv = Conv2D(filters, kernel_size=kernel_size, strides=strides, padding='same')(initial_layer)
         down_conv = LeakyReLU(alpha=0.2)(down_conv)
         down_conv = BatchNormalization(momentum=momentum)(down_conv) if momentum else down_conv
-
+        down_conv = Dropout(dropout_rate)(down_conv) if dropout_rate else down_conv
         return down_conv
 
     def deconv2d(self, initial_layer: Layer, skipped_layer: Layer, filters: int, kernel_size=4,
@@ -27,33 +27,33 @@ class GenerativeNetwork():
         up_conv = UpSampling2D(size=2)(initial_layer)
         up_conv = Conv2D(filters, kernel_size=kernel_size, strides=strides, padding='same', activation=activation)(
             up_conv)
-        up_conv = Dropout(dropout_rate)(up_conv) if dropout_rate else up_conv
         up_conv = BatchNormalization(momentum=momentum)(up_conv)
         up_conv = Concatenate()([up_conv, skipped_layer])
+        up_conv = Dropout(dropout_rate)(up_conv) if dropout_rate else up_conv
         return up_conv
 
-    # TODO to init
     def build(self, init_filters: int,
               input_shape: Tuple[int, int, int],
               output_channels: int,
-              batch_normalization_momentum=0.8,
+              momentum=0.8,
+              dropout_rate: Optional[float] = None,
               output_activation='tanh',
               compile: bool = True) -> Model:
         input = Input(shape=input_shape)
 
         d1 = self.conv2d(input, init_filters)
-        d2 = self.conv2d(d1, init_filters * 2, momentum=batch_normalization_momentum)
-        d3 = self.conv2d(d2, init_filters * 4, momentum=batch_normalization_momentum)
-        d4 = self.conv2d(d3, init_filters * 8, momentum=batch_normalization_momentum)
-        d5 = self.conv2d(d4, init_filters * 8, momentum=batch_normalization_momentum)
-        d6 = self.conv2d(d5, init_filters * 8, momentum=batch_normalization_momentum)
-        d7 = self.conv2d(d6, init_filters * 8, momentum=batch_normalization_momentum)
+        d2 = self.conv2d(d1, init_filters * 2, momentum=momentum, dropout_rate=dropout_rate)
+        d3 = self.conv2d(d2, init_filters * 4, momentum=momentum, dropout_rate=dropout_rate)
+        d4 = self.conv2d(d3, init_filters * 8, momentum=momentum, dropout_rate=dropout_rate)
+        d5 = self.conv2d(d4, init_filters * 8, momentum=momentum, dropout_rate=dropout_rate)
+        d6 = self.conv2d(d5, init_filters * 8, momentum=momentum, dropout_rate=dropout_rate)
+        d7 = self.conv2d(d6, init_filters * 8, momentum=momentum, dropout_rate=dropout_rate)
 
-        u1 = self.deconv2d(d7, d6, init_filters * 8)
-        u2 = self.deconv2d(u1, d5, init_filters * 8)
-        u3 = self.deconv2d(u2, d4, init_filters * 8)
-        u4 = self.deconv2d(u3, d3, init_filters * 4)
-        u5 = self.deconv2d(u4, d2, init_filters * 2)
+        u1 = self.deconv2d(d7, d6, init_filters * 8, momentum=momentum, dropout_rate=dropout_rate)
+        u2 = self.deconv2d(u1, d5, init_filters * 8, momentum=momentum, dropout_rate=dropout_rate)
+        u3 = self.deconv2d(u2, d4, init_filters * 8, momentum=momentum, dropout_rate=dropout_rate)
+        u4 = self.deconv2d(u3, d3, init_filters * 4, momentum=momentum, dropout_rate=dropout_rate)
+        u5 = self.deconv2d(u4, d2, init_filters * 2, momentum=momentum, dropout_rate=dropout_rate)
         u6 = self.deconv2d(u5, d1, init_filters)
         u7 = UpSampling2D(size=2)(u6)
 
--- scgan/gdal_operations.py
+++ scgan/gdal_operations.py
@@ -1,26 +1,34 @@
 import os
 import pandas as pd
 
-grid = 'grid.shp'
-sentinel = 'sentinel.tif'
-land_cover = 'land_cover.tif'
+dir = './'
 
-os.makedirs('dataset/S', exist_ok=True)
-os.makedirs('dataset/LC', exist_ok=True)
+grid = os.path.join(dir, 'grid_c.shp')
+sentinel = os.path.join(dir, 'sentinel.tif')
+land_cover = os.path.join(dir, 'land_cover.tif')
+
+dataset = os.path.join(dir, 'dataset')
+s_dataset = os.path.join(dataset, 'S')
+lc_dataset = os.path.join(dataset, 'LC')
+
+os.makedirs(s_dataset, exist_ok=True)
+os.makedirs(lc_dataset, exist_ok=True)
 
 effective_ids = []
 
 for i in range(1, 1000):
     code_s = os.system(
-        'gdalwarp -cutline %s -cwhere "id=%s" -tr 10 10 -crop_to_cutline %s dataset/S/S_%s.tif' % (
-            grid, i, sentinel, i))
+        'gdalwarp -cutline %s -cwhere "id=%s" -tr 10 10 -crop_to_cutline %s %s/S_%s.tif' % (
+            grid, i, sentinel, s_dataset, i))
     code_ls = os.system(
-        'gdalwarp -cutline %s -cwhere "id=%s" -tr 100 100 -crop_to_cutline %s dataset/LC/LC_%s.tif' % (
-            grid, i, land_cover, i))
+        'gdalwarp -cutline %s -cwhere "id=%s" -tr 100 100 -crop_to_cutline %s %s/LC_%s.tif' % (
+            grid, i, land_cover, lc_dataset, i))
 
     if code_s == 0 or code_ls == 0:
         effective_ids.append(i)
+    else:
+        break
 
 pd.DataFrame({
-    'id': list(range(1, 1000))
-}).to_csv('/tmp/data_descriptor.csv', index=False)
+    'id': effective_ids
+}).to_csv('%s/data_descriptor.csv' % dataset, index=False)
